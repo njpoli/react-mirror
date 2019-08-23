b72d330ed09b19b0f71f47728df297cd8d838d1b
unknown <nathaniel.poli@gmail.com>
2019-08-23 17:15:05 -0400

    added some default users to generate in mongo

--- server/routes/generate-data.js
+++ server/routes/generate-data.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Compliment = require('../models/compliment');
+const User = require('../models/user')
 
 const defaultCompliments = [
   {
@@ -20,6 +21,44 @@ const defaultCompliments = [
   }
 ]
 
+const defaultUsers = [
+  {
+    name: 'Nate',
+    widgets: [{
+      name: 'Clock',
+      location: 'region left'
+    },
+    {
+      name: 'Compliment',
+      location: 'region lower third'
+    },
+    {
+      name: 'Twitch',
+      location: 'region bar'
+    },
+    {
+      name: 'Weather',
+      location: 'region bottom'
+    }]
+  },
+  {
+    name: 'Krysti',
+    widgets: [{
+        name: 'Clock',
+        location: 'region right'
+      },
+      {
+        name: 'Weather',
+        location: 'region left'
+      },
+      {
+        name: 'Compliment',
+        location: 'region lower third'
+      }
+    ]
+  }
+]
+
 router.get('/compliments', (req, res, next) => {
   defaultCompliments.forEach(complimentItem => {
     let compliment = new Compliment();
@@ -31,6 +70,19 @@ router.get('/compliments', (req, res, next) => {
     })
   })
   res.status(200).send('Compliments saved!')
+})
+
+router.get('/users', (req, res, next) => {
+  defaultUsers.forEach(userItem => {
+    let user = new User();
+    user.name = userItem.name;
+    user.widgets = userItem.widgets;
+
+    user.save((err) => {
+      if (err) throw err;
+    })
+  })
+  res.status(200).send('Users saved!')
 })
 
 module.exports = router;
