ae51236fbcc1aaff0c61d0e60f9ae7d80ab34a62
unknown <nathaniel.poli@gmail.com>
2019-08-23 17:55:14 -0400

    fixed name of users route, added get users route to server

--- server/server.js
+++ server/server.js
@@ -15,9 +15,11 @@ app.use(bodyParser.urlencoded({
 
 const dataRoutes = require('./routes/generate-data');
 const complimentRoutes = require('./routes/compliments');
+const userRoutes = require('./routes/users')
 
 app.use('/data', dataRoutes);
 app.use('/compliments', complimentRoutes);
+app.use('/users', userRoutes);
 
 const port = 8000;
 const server = http.createServer(app);
